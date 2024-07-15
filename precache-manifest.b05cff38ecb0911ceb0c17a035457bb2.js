e651668c6789a0c0a916bb72382c28aa7f13b38d
Saurav-Shinde <sauravshindee26@gmail.com>
2024-07-15 22:01:47 +0530

    Updates

---
+++ precache-manifest.b05cff38ecb0911ceb0c17a035457bb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8514c34e6836e836567fac8305705c",
+    "url": "/MeowsWoofs/index.html"
+  },
+  {
+    "revision": "5ccae6980dcea3d0572f",
+    "url": "/MeowsWoofs/static/css/main.0647e2eb.chunk.css"
+  },
+  {
+    "revision": "3f470e40d540d2ce64f2",
+    "url": "/MeowsWoofs/static/js/2.6edf4f87.chunk.js"
+  },
+  {
+    "revision": "2f58089c61d46f50cf6fed08f3e9f0eb",
+    "url": "/MeowsWoofs/static/js/2.6edf4f87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba12d81fc17bd09ec136",
+    "url": "/MeowsWoofs/static/js/3.19599855.chunk.js"
+  },
+  {
+    "revision": "5ccae6980dcea3d0572f",
+    "url": "/MeowsWoofs/static/js/main.a17186df.chunk.js"
+  },
+  {
+    "revision": "fbfd57b8c7731281abf1",
+    "url": "/MeowsWoofs/static/js/runtime-main.766f1344.js"
+  }
+]);
